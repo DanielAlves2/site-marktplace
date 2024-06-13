6eabd2bbbd00e3b1e79c7f0768873409df5b3081
dansfab <danielfabricio199@gmail.com>
2024-06-13 16:01:56 -0300

    Create aula03.js

---
+++ aula03.js
@@ -0,0 +1,11 @@
+const idade = 6;
+const nome = 'Julio';
+
+
+if(idade>=18 || nome ==='Julio' ){
+    console.log('Pode entrar Sr ', nome);
+} else{
+    console.log('Não entra!')
+}
+
+//console.log('usuario pode entrar',podeEntrar )
