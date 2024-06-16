1535aa787044d23870658aa279bef0e6ec2c141a
dansfab <danielfabricio199@gmail.com>
2024-06-16 00:18:47 -0300

    strings

---
+++ aula14.js
@@ -0,0 +1,14 @@
+const mensagem = 'opai osid';
+const mensagem2 = 'saigshhguidhghhk';
+
+console.log('tamanho: ', mensagem.length);
+console.log('Concat', mensagem.concat(mensagem2));
+console.log('Concat +', "ouytr" + " " + mensagem + "" ( mensagem2 ));
+console.log(mensagem.split('i'))
+console.log(mensagem.substring(2,4))
+console.log('ES:', 'dsaklfj ${mensagem} ${mensagem2}');
+console.log('Quebrar em array', mensagem.split(' '));
+
+console.log('UP:', mensagem.toUpperCase());//tudo vira maiusculo
+console.log('DOWN:', mensagem.toLowerCase());//tudo para minusculo
+console.log('Replace: ', mensagem.replace('opai osid', 'Joao' ));
